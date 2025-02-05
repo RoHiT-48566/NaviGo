ab9b338f02dcbcd0277bff5cd69ce3f6e570bba3
RoHiT-48566 <m.rohit48566@gmail.com>
2025-02-06 00:05:56 +0530

    started integrating login and signup with backend routes

--- frontend/src/pages/UserSignup.jsx
+++ frontend/src/pages/UserSignup.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { UserDataContext } from "../context/userContext";
 
 const UserSignup = () => {
   const [email, setEmail] = useState("");
@@ -9,16 +11,39 @@ const UserSignup = () => {
   const [lastName, setLastName] = useState("");
   const [userData, setUserData] = useState({});
 
-  const submitHandler = (e) => {
+  const navigate = useNavigate();
+
+  const { user, setUser } = React.useContext(UserDataContext);
+
+  const submitHandler = async (e) => {
     e.preventDefault();
-    setUserData({
+    const newUser = {
       fullName: {
         firstName: firstName,
         lastName: lastName,
       },
       email: email,
       password: password,
-    });
+    };
+
+    const response = await axios.post(
+      `${import.meta.env.VITE_BASE_URL}/user/register`,
+      newUser
+    );
+    if (response.status == 201) {
+      const data = response.data;
+      setUser(data.user);
+      navigate("/home");
+    }
+
+    // setUserData({
+    //   fullName: {
+    //     firstName: firstName,
+    //     lastName: lastName,
+    //   },
+    //   email: email,
+    //   password: password,
+    // });
     console.log(userData);
     setEmail("");
     setPassword("");

0f1932fb3ec81a5c658073c5dcae1a674a55acea
RoHiT-48566 <m.rohit48566@gmail.com>
2025-02-15 16:15:31 +0530

    Auto Address Suggestions is added.

--- Backend/controllers/map.controller.js
+++ Backend/controllers/map.controller.js
@@ -29,3 +29,18 @@ module.exports.getDistanceTime = async (req, res, next) => {
     res.status(404).json({ message: "Couldn't find Distance and Time" });
   }
 };
+
+module.exports.getAutoCompleteSuggestions = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { input } = req.query;
+    const suggestions = await mapService.getAutoCompleteSuggestions(input);
+    res.status(200).json(suggestions);
+  } catch (err) {
+    console.error(err);
+    res.status(404).json({ message: "Couldn't get any suggestions" });
+  }
+};

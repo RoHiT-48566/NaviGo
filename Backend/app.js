dda3a2a2c07f6bfb70359c4c77317352122e4110
RoHiT-48566 <m.rohit48566@gmail.com>
2025-02-16 20:14:21 +0530

    Fare calculation is added (backend)

--- Backend/app.js
+++ Backend/app.js
@@ -13,6 +13,7 @@ const connectToDb = require("./db/db");
 const userRoutes = require("./routes/user.routes");
 const captainRoutes = require("./routes/captain.routes");
 const mapRoutes = require("./routes/maps.routes");
+const rideRoutes = require("./routes/ride.routes");
 
 connectToDb();
 
@@ -28,5 +29,6 @@ app.get("/", (req, res) => {
 app.use("/users", userRoutes);
 app.use("/captains", captainRoutes);
 app.use("/maps", mapRoutes);
+app.use("/rides", rideRoutes);
 
 module.exports = app;
